717d902b41eb61eb1129354203082388d74f431b
Rafael Honorato <114627686+rafaelhonorato03@users.noreply.github.com>
2026-02-26 19:34:49 -0300

    Update semana_05.py

---
+++ semana_05.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import skfuzzy as fz
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+
+plt.pcParams['figure.figsize'] = (15, 10)
